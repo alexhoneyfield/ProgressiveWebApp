c0b2326bfdb1b757853c26aeb1b8193f52be7d38
Alex <alex.honeyfield@gmail.com>
2021-06-02 17:09:35 +1200

    Build

--- service-worker.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('fetch', (e) => {
-  e.request.url = '/build/' + e.request.url
-  console.log(e.request.url);
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request)),
-  );
-});
-
-self.__WB_MANIFEST
--- src/service-worker.js
+++ src/service-worker.js
@@ -1,5 +1,4 @@
 self.addEventListener('fetch', (e) => {
-  e.request.url = '/build/' + e.request.url
   console.log(e.request.url);
   e.respondWith(
     caches.match(e.request).then((response) => response || fetch(e.request)),
