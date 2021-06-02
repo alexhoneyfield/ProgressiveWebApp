9dfe3a7701ba04278b18aeabfaa412b75491abe1
Alex <alex.honeyfield@gmail.com>
2021-06-02 17:01:05 +1200

    Build

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,4 +1,5 @@
 self.addEventListener('fetch', (e) => {
+  e.request.url = '/build/' + e.request.url
   console.log(e.request.url);
   e.respondWith(
     caches.match(e.request).then((response) => response || fetch(e.request)),
